84d6a3ab6a1c833e84c9a78eb529e92139822f47
Maria Gamal <maria.gamal13@gmail.com>
2023-02-06 19:55:36 +0200

    phase 1 parser done

--- parser/test/unrolled.c
+++ parser/test/unrolled.c
@@ -1,5 +1,6 @@
 int main() {
   int j;
+  int x = 0;
   int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
   // for (int i = 2; i < 5; i += 1) {
@@ -12,25 +13,31 @@ int main() {
   //   }
   // }
 
-  for (int i = 1; i < 5; i += 1) {
-    arr[i] = arr[i + 1];
-  }
+  // for (int i = 1; i < 5; i += 1) {
+  //   arr[i] = arr[i + 1];
+  // }
 
-  for (int i = 1; i < 10; i += 2) {
-    j = i + 2;
+  for (int i = 0; i < 10; i += 2) {
+    j = i + 1;
+    x = x + 1;
     arr[i] = arr[j];
 
-    j = (i + 1) + 2;
+    j = (i + 1) + 1;
+    x = x + 1;
     arr[i + 1] = arr[j];
   }
 
-  for (int i = 15; i < 25; i += 3) {
-    j = i + 2;
-    arr[i] = arr[j];
+  // 0 1
+  // 1 2 
+  // ; -%main.19.offset+%main.10.offset <= 20;
 
-    j = (i + 1) + 2;
-    arr[i + 1] = arr[j];
-  }
+  // for (int i = 15; i < 25; i += 3) {
+  //   j = i + 2;
+  //   arr[i] = arr[j];
+
+  //   j = (i + 1) + 2;
+  //   arr[i + 1] = arr[j];
+  // }
 
   return 0;
 }
